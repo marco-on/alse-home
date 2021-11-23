8b3ade30cbccfa081529222e0740341565b9095b
Marco Ortiz N <marco.ortiz@escuelaing.edu.co>
2021-11-23 13:42:57 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -24,7 +24,7 @@ window.addEventListener('DOMContentLoaded', event => {
     }
 
     //setting document location to about-alse when loading site
-    window.location.pathname = '#id-about-alse';
+    document.location.pathname = '#id-about-alse';
     //window.location.assign('#id-about-alse');
 
 });
