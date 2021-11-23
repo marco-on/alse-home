482f34c28c936a43b547b4c604004d4b0b9903a5
Marco Ortiz N <marco.ortiz@escuelaing.edu.co>
2021-11-23 12:40:55 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -24,6 +24,6 @@ window.addEventListener('DOMContentLoaded', event => {
     }
 
     //setting document location to about-alse when loading site
-    document.location.hash = 'id-about-alse';
+    window.location.hash = '#id-about-alse';
 
 });
